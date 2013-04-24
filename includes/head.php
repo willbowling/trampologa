26cd527feef3377849af573d54ecd96791a85489
Will Bowling <tiltedcircle@gmail.com>
2013-04-23 20:46:11 -0400

    adding stuff

---
+++ includes/head.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Power Trampologa</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/screen.css">
+    <link rel="stylesheet" href="css/pygments.css" />
+    <!-- <link rel="stylesheet" href="css/slideshow.css" /> -->
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
---
+++ includes/nav.php
@@ -0,0 +1,11 @@
+                        <nav class="main-nav clearfix">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="physical-therapy.html">Physical Therapy</a></li>
+                                <!-- <li><a href="#">Media</a></li> -->
+                                <!-- <li><a href="#">Shop</a></li> -->
+                                <!-- <li><a href="#">About</a></li> -->
+                                <li><a href="contact.html">Contact</a></li>
+                                <!-- <li><a href="#">Class Schedule</a></li> -->
+                            </ul>
+                        </nav>
